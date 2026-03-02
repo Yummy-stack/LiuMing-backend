d8526df6fe1db58dcaefc41a68c7b058599aedb9
Yummy <2512061959@qq.com>
2026-03-02 16:11:09 +0800

    其他（后端） - 项目初始化

---
+++ LiuMing-services/src/main/java/com/liumingservices/LiuMingServicesApplication.java
@@ -0,0 +1,14 @@
+package com.liumingservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiuMingServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiuMingServicesApplication.class, args);
+        System.out.println("\n ----------------- 项目启动成功 ----------------- \n");
+    }
+
+}
