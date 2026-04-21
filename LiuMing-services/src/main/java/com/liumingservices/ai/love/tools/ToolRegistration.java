c1609327467cdaf1f2913ad94b3da7a4cc7a884b
Yummy <2512061959@qq.com>
2026-04-21 21:40:29 +0800

    FileOperation工具的实现

---
+++ LiuMing-services/src/main/java/com/liumingservices/ai/love/tools/ToolRegistration.java
@@ -0,0 +1,20 @@
+package com.liumingservices.ai.love.tools;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.tool.ToolCallback;
+import org.springframework.ai.tool.ToolCallbacks;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class ToolRegistration {
+    @Bean
+    public ToolCallback[] allTools() {
+        FileOperationTool fileOperationTool = new FileOperationTool();
+
+        return ToolCallbacks.from(
+                fileOperationTool
+        );
+    }
+}
